78b6c55eda2f4c2e7656c7c0add28ebb0958d820
Rashmi Dixit <57787129+rashmi-dixit@users.noreply.github.com>
2020-05-15 07:56:45 +0530

    Revert "Merging to Master (#1)"
    This reverts commit b88c94469bfadf5fde7956f754ede54d0267d031.

--- Source/Microsoft.Teams.Apps.FAQPlusPlus.Configuration/Models/HelpTabViewModel.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus.Configuration/Models/HelpTabViewModel.cs
@@ -13,10 +13,10 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Configuration.Models
         /// <summary>
         /// Gets or sets help tab message text box to be used in View
         /// </summary>
-        [Required(ErrorMessage = "輸入幫助標籤文字。")]
-        [StringLength(maximumLength: 3000, ErrorMessage = "幫助標籤文字應少於3000個字符。", MinimumLength = 2)]
+        [Required(ErrorMessage = "Enter help tab text.")]
+        [StringLength(maximumLength: 3000, ErrorMessage = "Help tab text should contain less than 3000 characters.", MinimumLength = 2)]
         [DataType(DataType.Text)]
-        [Display(Name = "幫助標籤文字")]
+        [Display(Name = "Help tab text")]
         public string HelpTabText { get; set; }
     }
 }
